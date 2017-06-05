99fddd045ad0518985a8095b694fc551a04e58d2
Guilherme Freitas <guilhermeddf@outlook.com>
2017-06-05 18:21:41 -0300

    controllers institutions and volunteers

--- app/controllers/volunteer_controller.rb
+++ app/controllers/volunteer_controller.rb
@@ -1,2 +1,21 @@
 class VolunteerController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 end
